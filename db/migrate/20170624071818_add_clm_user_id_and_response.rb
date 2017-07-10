5793e47c613e308e9fe815a2a2c8d471af95cd73
Caroline Karanja <ckaranja24@gmail.com>
2017-07-09 23:10:14 -0500

    added a user id to videos

--- db/migrate/20170624071818_add_clm_user_id_and_response.rb
+++ db/migrate/20170624071818_add_clm_user_id_and_response.rb
@@ -1,6 +1,5 @@
 class AddClmUserIdAndResponse < ActiveRecord::Migration
   def change
-    add_column :videos, :user_id, :integer
     add_column :videos, :response, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416030002) do
+ActiveRecord::Schema.define(version: 20170710040625) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "video_id"
+    t.integer  "question_id"
+    t.string   "question_name"
+    t.text     "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "video_response_id"
+    t.string   "question_type"
+  end
+
+  create_table "interactions", force: :cascade do |t|
+    t.integer  "video_id"
+    t.integer  "question_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "interaction_at",        default: 0
+    t.integer  "interaction_at_offset", default: 0
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "video_id"
+    t.string   "question_label"
+    t.string   "question_type"
+    t.string   "choice_1"
+    t.string   "choice_2"
+    t.string   "choice_3"
+    t.string   "choice_4"
+    t.string   "button_text"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "btn_url"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,10 +67,20 @@ ActiveRecord::Schema.define(version: 20170416030002) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  create_table "video_responses", force: :cascade do |t|
+    t.integer  "video_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "videos", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "video_link"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "slug"
+    t.integer  "response",   default: 0
+    t.string   "video_type"
+    t.integer  "user_id"
   end
 
 end
